52e9a8e94fa5262b060d82486423de552aeb3bb7
Oscar Cristaldo <fabianskier@icloud.com>
2022-08-10 19:54:15 -0400

    Initial Commit

---
+++ TopContributors/TopContributorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopContributorsApp.swift
+//  TopContributors
+//
+//  Created by Oscar Cristaldo on 2022-08-10.
+//
+
+import SwiftUI
+
+@main
+struct TopContributorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
